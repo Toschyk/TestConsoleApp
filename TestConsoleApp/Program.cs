737ad9911741616a87d52709755e2ca64effe350
Toschyk <toschyk@icloud.com>
2024-11-17 21:23:03 +0700

    Написан код тестового консольного приложения

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -10,6 +10,37 @@ namespace TestConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите первое число:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите операцию (+, -, *, /):");
+            char operation = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = 0;
+
+            switch (operation)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+                default:
+                    Console.WriteLine("Некорректная операция.");
+                    break;
+            }
+
+            Console.WriteLine($"Результат: {result}");
         }
     }
 }
